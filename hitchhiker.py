18452045c8b248fdb54c8ece0337a3e0c2824ea3
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-27 19:40:16 -0400

    Updated from Brython Server: 9/27/2017 7:40:15 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,3 +1,9 @@
 #Greg Phillips
 #9/27/17
-#hitchhiker.py - 
+#hitchhiker.py - asks question and answers 42 or quits
+
+for i in range(1,100):
+    question = input("Enter a question or type quit: ")
+    if "quit" not in question:
+        print("42")
+        
