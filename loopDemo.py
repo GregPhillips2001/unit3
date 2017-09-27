4582cf2e8096edcc0996c65a9b4a94b283626701
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-27 13:28:44 -0400

    Updated from Brython Server: 9/27/2017 1:28:43 PM

--- loopDemo.py
+++ loopDemo.py
@@ -1,1 +1,12 @@
+#Greg Phillips
+#9/27/19
+#loopDemo.py - intro to loops
 
+"""
+#print I love computer science 5 times
+for i in range(1,6):
+    print("I love computer science")"""
+
+#Print the numbers from 1 to 20
+for i in range(1,21):
+    print(i)
