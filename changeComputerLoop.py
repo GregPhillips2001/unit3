eb9a6cca713dee1e16c970ede7e10e66084cabbc
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-02 14:48:52 -0400

    Updated from Brython Server: 10/2/2017 2:48:51 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,16 @@
+#Greg Phillips
+#10/2/17
+#changeComputerLoop.py 
 
+change = int(input("Enter the number of change you need: "))
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+
+for i in range(1,change+1):
+    if i-quarter < 0:
+        print("0 quarters")
+        if i-dime < 0:
+            print
+    
