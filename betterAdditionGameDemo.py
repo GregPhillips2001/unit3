2840250200c5811e03b87912345e1df7be61d294
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-04 13:34:53 -0400

    Updated from Brython Server: 10/4/2017 1:34:53 PM

--- betterAdditionGameDemo.py
+++ betterAdditionGameDemo.py
@@ -1,1 +1,14 @@
+#Greg Phillips
+#10/4/17
+#betterAdditionGame.py - ask addition problems until user gets 5
+
+from random import randint
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    question = "what is " + str(num1) + "+" +str(num2) + "?"
+    answer = int(input(question))
+    
 
