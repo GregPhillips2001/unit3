8a4d73593f4a2d226394531ffc04a84aa69ab4e1
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-05 14:40:35 -0400

    Updated from Brython Server: 10/5/2017 2:40:34 PM

--- warmup10.py
+++ warmup10.py
@@ -1,1 +1,27 @@
+#Greg Phillips
+#9/27/17
+#satfb.py
 
+from ggame import * 
+
+red = Color(0xcc0000,1)
+blue = Color(0x000099,1)
+white = Color(0xffffff,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(1,black) #(pixels thick,color)
+
+redRectangle = RectangleAsset(100,20,blackOutline,red) #(width,height,outline,fill)
+whiteRectangle = RectangleAsset(100,20,blackOutline,white) #(width,height,outline,fill)
+blueRectangle = RectangleAsset(100,20,blackOutline,blue) #(width,height,outline,fill)
+text = TextAsset("SATURDAYS", fill=white, style="bold 5pt Times")
+text2 = TextAsset("ARE_FOR", fill=blue, style="bold 5pt Times")
+text3 = TextAsset("THE_BOYS", fill=white, style="bold 5pt Times")
+
+Sprite(redRectangle)
+Sprite(whiteRectangle, (0,20))
+Sprite(blueRectangle, (0,40))
+Sprite(text, (10,2))
+Sprite(text2, (10,7))
+Sprite(text3, (10,12))
+App().run()
